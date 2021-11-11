ea6b98ecb64e46649bb42463002b6ad75967d748
mborges-pivotal <marcelo.marco.borges@gmail.com>
2021-11-11 03:34:38 -0600

    using datastax cqlsh image

--- concourse/tasks/cqlsh.sh
+++ concourse/tasks/cqlsh.sh
@@ -14,4 +14,3 @@ secure_connect_bundle_url=$(cat terraform/metadata | ./jq -r '.secure_connect_bu
 wget -O secure_connect.zip $secure_connect_bundle_url
 
 # Run CQLSH
-tar -xzvf concourse/tasks/cqlsh-astra-20201104-bin.tar.gz
