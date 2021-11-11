3ec423a971de0a4bfab645d9047bf4b29ea38aa7
mborges-pivotal <marcelo.marco.borges@gmail.com>
2021-11-11 03:18:23 -0600

    cqlsh fixes

--- concourse/tasks/cqlsh.sh
+++ concourse/tasks/cqlsh.sh
@@ -5,8 +5,13 @@ set -e
 echo "name: $(cat terraform/name)"
 echo "metadata: $(cat terraform/metadata)"
 
-secure_connect_bundle_url=$(cat terraform/metadata | ./jq -r '.secure_connect_bundle_url')
+# Get JQ
+wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+chmod +x ./jq
 
+# Download secure bundle
+secure_connect_bundle_url=$(cat terraform/metadata | ./jq -r '.secure_connect_bundle_url')
 wget -O secure_connect.zip $secure_connect_bundle_url
 
+# Run CQLSH
 tar -xzvf concourse/tasks/cqlsh-astra-20201104-bin.tar.gz
