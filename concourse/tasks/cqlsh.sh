0e3d08d1194904ffde4a80acc1a629fb1316fa6b
mborges-pivotal <marcelo.marco.borges@gmail.com>
2021-11-11 08:19:36 -0600

    cqlsh task fixes

--- concourse/tasks/cqlsh.sh
+++ concourse/tasks/cqlsh.sh
@@ -13,5 +13,10 @@ chmod +x $HOME/jq
 secure_connect_bundle_url=$(cat terraform/metadata | $HOME/jq -r '.secure_connect_bundle_url')
 wget -O $HOME/secure_connect_bundle.zip $secure_connect_bundle_url
 
+# Install CQLSH for Astra
+mv concourse/tasks/ccqlsh-astra-20201104-bin.tar.gz $HOME
+cd
+tar -xvf ccqlsh-astra-20201104-bin.tar.gz
+
 # Run CQLSH
 /usr/share/cqlclient/cqlsh -u $ASTRA_CLIENT_ID -p $ASTRA_CLIENT_SECRET -b $HOME/secure_connect_bundle.zip -f create_model.cql
