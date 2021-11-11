bee52126c4ccb6bb8029d1180be436266796994e
mborges-pivotal <marcelo.marco.borges@gmail.com>
2021-11-11 02:57:04 -0600

    cqlsh task improvements

--- concourse/tasks/cqlsh.sh
+++ concourse/tasks/cqlsh.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# set -e
+set -e
 
 echo "name: $(cat terraform/name)"
 echo "metadata: $(cat terraform/metadata)"
+
+secure_connect_bundle_url=$(cat terraform/metadata | ./jq -r '.secure_connect_bundle_url')
+
+wget -O secure_connect.zip $secure_connect_bundle_url
+
+tar -xzvf concourse/tasks/cqlsh-astra-20201104-bin.tar.gz
